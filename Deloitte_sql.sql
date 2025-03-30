e9a441eb067555fac8841ee3692c18da2ba6d357
3243fwdf <samantaarijit798@gmail.com>
2025-03-30 15:11:11 +0530

    Update Deloitte_sql.sql

--- Deloitte_sql.sql
+++ Deloitte_sql.sql
@@ -1,3 +1,6 @@
+--Based on the user_activity table, write a query to
+--retrieve the time difference between each user's
+--most recent event and their second-to-last event, sorted in ascending order by user_identifier.
 use deliotte
 
 -- Creare Table user_activity
